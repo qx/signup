d2938515d622b3d75bf57be89caa0a8bbd99d31f
ok <qx.ouyang@gmail.com>
2015-01-02 17:45:22 +0800

    2015年 1月 2日 星期五 17时45分11秒 CST
      Content:更新首页测试用例.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,32 +8,38 @@ describe 'StaticPages' do
   #     response.status.should be(200)
   #   end
   # end
+
   describe 'Home page' do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it 'should have the base title' do
+      visit '/static_pages/home'
+      page.should have_title('Ruby on Rails Tutorial Sample App')
+    end
+
+    it 'should not have a custom page title' do
+      visit '/static_pages/home'
+      page.should_not have_title('| Home')
     end
   end
-  describe 'Help page' do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
-  end
-  describe 'About page' do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
-  end
-
-  it 'should have the right title' do
-    visit '/static_pages/home'
-    # page.should have_selector('title',:text => 'WELCOME')
-    page.should have_content('WELCOME')
-  end
-
+  # describe 'About page' do
+  #
+  #   it "should have the content 'About Us'" do
+  #     visit '/static_pages/about'
+  #     page.should have_content('About Us')
+  #   end
+  # end
+  # describe ' contact page test ' do
+  #
+  #   it ' should have the right title' do
+  #     visit '/static_pages/contact'
+  #     # page.should have_selector('title',:text => 'WELCOME')
+  #     page.should have_content('Contact')
+  #   end
+  #
+  # end
 end
