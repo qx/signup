365b8b375c908e5d1af4e87f503618425646b2ca
ok <qx.ouyang@gmail.com>
2015-01-02 13:44:08 +0800

    2015年 1月 2日 星期五 13时44分00秒 CST
      Content:增加标题测试.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,14 +19,21 @@ describe "StaticPages" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('hppp')
+      page.should have_content('Help')
     end
   end
   describe "About page" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('Aboutou onceyoudf')
+      page.should have_content('About Us')
     end
   end
+
+  it "should have the right title" do
+    visit '/static_pages/home'
+    page.should have_selector('title',
+                              :text => "home title")
+  end
+
 end
