ef6d1ec9aef0975ed0d2e1a89d535dd66acd6229
ok <qx.ouyang@gmail.com>
2015-01-02 16:23:16 +0800

    2015年 1月 2日 星期五 16时23分03秒 CST
      Content:修改主模板.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe 'StaticPages' do
   # describe "GET /static_pages/home" do
   #   it "works! (now write some real specs)" do
   #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -8,21 +8,21 @@ describe "StaticPages" do
   #     response.status.should be(200)
   #   end
   # end
-  describe "Home page" do
+  describe 'Home page' do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
   end
-  describe "Help page" do
+  describe 'Help page' do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
   end
-  describe "About page" do
+  describe 'About page' do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
@@ -30,10 +30,10 @@ describe "StaticPages" do
     end
   end
 
-  it "should have the right title" do
+  it 'should have the right title' do
     visit '/static_pages/home'
-    page.should have_selector('title',
-                              :text => "home title")
+    # page.should have_selector('title',:text => 'WELCOME')
+    page.should have_content('WELCOME')
   end
 
 end
