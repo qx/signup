2fdfaa24b10d517f9fa8916c833222b6a86a4e50
ok <qx.ouyang@gmail.com>
2015-01-03 12:43:54 +0800

    简化测试代码
     subject { page }
    
    it {}

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,58 +26,44 @@ describe 'StaticPages' do
   #     page.should_not have_title('| Home')
   #   end
   # end
-
+  subject { page }
   describe 'Home page' do
-    before {visit root_path}
-    it "should have the h1 'Sample App'" do
-      page.should have_selector('h1', text: 'Sample App')
-    end
+    before { visit root_path }
+    # it "should have the h1 'Sample App'" do
+    #   page.should have_selector('h1', text: 'Sample App')
+    # end
 
-    it 'should have the base title' do
-      page.should have_title('Ruby on Rails Tutorial Sample App')
-    end
+    it { should have_selector('h1', text: 'Sample App') }
 
-    it 'should not have a custom page title' do
-      page.should_not have_title( '| Home')
-    end
+    # it 'should have the base title' do
+    #   page.should have_title('Ruby on Rails Tutorial Sample App')
+    # end
+    it { should have_title('Ruby on Rails Tutorial Sample App') }
+
+    # it 'should not have a custom page title' do
+    #   page.should_not have_title('| Home')
+    # end
+    it { should_not have_title('| Home') }
   end
 
   describe 'Help page' do
+    before { visit help_path }
+    it { should have_selector('h1', text: 'Help') }
 
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', text: 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_title('Ruby on Rails Tutorial Sample App | Help')
-    end
+    it { should have_title('Ruby on Rails Tutorial Sample App | Help') }
   end
 
   describe 'About page' do
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About Us') }
 
-    it "should have the h1 'About'" do
-      visit about_path
-      page.should have_selector('h1', text: 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      page.should have_title('Ruby on Rails Tutorial Sample App | About Us')
-    end
+    it { should have_title('Ruby on Rails Tutorial Sample App | About Us') }
   end
 
   describe 'Contact page' do
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
 
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_title('Ruby on Rails Tutorial Sample App | Contact')
-    end
+    it { should have_title('Ruby on Rails Tutorial Sample App | Contact') }
   end
 end
